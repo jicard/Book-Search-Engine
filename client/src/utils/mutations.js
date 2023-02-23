eebc83aef9b59825128ae252d291971d0e361ecd
Josh Icard <joshicard@Joshs-MacBook-Pro.local>
2023-02-23 17:42:27 -0500

    no good explanation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,87 @@
+import { gql } from "@apollo/client";
+//import gql from "graphql-tag";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String, $password: String) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          image
+          link
+          title
+          description
+        }
+      }
+      token
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $bookId: String
+    $authors: [String]
+    $description: String
+    $title: String
+    $image: String
+    $link: String
+  ) {
+    saveBook(
+      bookId: $bookId
+      authors: $authors
+      description: $description
+      title: $title
+      image: $image
+      link: $link
+    ) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
